cc39c299d49024e30205bcb0356685f9f54e2a2c
Ольга Бокатая <obyk>
2018-09-04 22:22:47 +1000

    фильтрация tasklist

--- homemade4.py
+++ homemade4.py
@@ -1,44 +1,56 @@
+## -*- coding: utf-8 -*-
 import socket
 import time
 import subprocess
 import csv
+import os
 
 def GetHostName():
-	print (socket.gethostname())
+  print (socket.gethostname())
 
 def GetDate():
-	print (time.strftime("%d/%m/%Y"))
+  print (time.strftime("%d/%m/%Y"))
 
 def GetIP():
-	Ip = socket.gethostbyname(socket.getfqdn())
-	IpArray = Ip.split ('.')
-	i = 1
-	for element in IpArray:
-		print (i, '-', element)
-		i+=1
+  Ip = socket.gethostbyname(socket.getfqdn())
+  IpArray = Ip.split ('.')
+  i = 1
+  for element in IpArray:
+    print (i, '-', element)
+    i+=1
 
 def GetPs():
-	Ps = subprocess.Popen('tasklist.exe /fo csv', stdout=subprocess.PIPE, universal_newlines=True)
-	tasks = subprocess.check_output(['tasklist']).decode('cp866', 'ignore').split("\r\n")
-	reader = csv.reader(tasks)
-	for row in reader:
-		print(" ".join(row))
+  tasks = subprocess.check_output(['tasklist']).decode('cp866', 'ignore').split("\r\n")
+  reader = csv.reader(tasks)
+  for row in reader:
+    print(" ".join(row))
+
+def PsFilter():
+  print ("Введите имя процесса")
+  exeName = input ()
+  tasks = subprocess.check_output(['tasklist']).decode('cp866', 'ignore').split("\r\n")
+  reader = csv.reader(tasks)
+  for row in reader:
+          process = " ".join(row)
+          if exeName in process: print (process)
 
 
 x = 1
 while x == 1:
-	print ("Введите команду")
-	command = input()
-	if command == 'gethostname':
-		GetHostName()
-	elif command == 'getdate':
-		GetDate()
-	elif command == 'getip':
-		GetIP()
-	elif command == 'exit':
-		x = 2
-	elif command == 'getps':
-		GetPs()
-	else: print ("Неверная команда!")
+  print ("Введите команду")
+  command = input()
+  if command == 'gethostname':
+    GetHostName()
+  elif command == 'getdate':
+    GetDate()
+  elif command == 'getip':
+    GetIP()
+  elif command == 'exit':
+    x = 2
+  elif command == 'getps':
+    GetPs()
+  elif command == 'filter':
+    PsFilter()
+  else: print ("Неверная команда!")
 
 print ("До скорой встречи!")
